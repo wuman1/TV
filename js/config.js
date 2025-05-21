19f2ec92dc85b587110e43b7e31497e36bf0e595
Skycnhe <105111832+Skycnhe@users.noreply.github.com>
2025-05-21 09:19:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -159,7 +159,8 @@ const API_SITES = {
     hsex: {
          api: 'https://hsex.icu/api.php/provide/vod',
          name: '好色TV资源',
-         adult: true
+         adult: true,
+         detail: 'https://hsex.icu
      },
 
     // 下面是资源失效率高的API源，不建议使用
