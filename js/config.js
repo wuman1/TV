e709c870117c31b4d11ae816768dfd7ff4b4a71b
xmynscnq <xmynscnq@gmail.com>
2025-06-17 09:52:27 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -124,15 +124,15 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
-        api: 'https://ckzy.me/api.php/provide/vod/?ac=list',
+        api: 'https://ckzy.me/api.php/provide/vod',
         name: 'CK资源',
         adult: true
     },
-    // jkun: {
-    //     api: 'https://jkunzyapi.com',
-    //     name: 'jkun资源',
-    //     adult: true
-    // },
+    jkun: {
+        api: 'https://jkunzyapi.com/api.php/provide/vod',
+        name: 'jkun资源',
+        adult: true
+    },
     // bwzy: {
     //     api: 'https://api.bwzym3u8.com',
     //     name: '百万资源',
