56bdb15ae0d8dc4550c290056521bd7ecc7f29ba
miaolink <167941042+miaolink@users.noreply.github.com>
2025-07-06 19:32:21 +0800

    Update version-check.js

--- js/version-check.js
+++ js/version-check.js
@@ -37,8 +37,8 @@ async function checkForUpdates() {
         // 获取最新版本
         let latestVersion;
         const VERSION_URL = {
-            PROXY: 'https://raw.ihtw.moe/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
-            DIRECT: 'https://raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt'
+            PROXY: 'https://xx.moe/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
+            DIRECT: 'https://xx.com/LibreSpark/LibreTV/main/VERSION.txt'
         };
         const FETCH_TIMEOUT = 1500;
         
