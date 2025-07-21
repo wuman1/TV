25ee8d6bd6e9ca9efae7a3cb2f4f44f07d1c9323
returneezeng <lizengster@gmail.com>
2025-07-21 21:01:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -125,38 +125,31 @@ const API_SITES = {
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
         api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
+        name: 'CK资源'
     },
     jkun: {
         api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
-        adult: true
+        name: 'jkun资源'
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
-        name: '百万资源',
-        adult: true
+        name: '百万资源'
     },
     souav: {
         api: 'https://api.souavzy.vip',
-        name: 'souav资源',
-        adult: true
+        name: 'souav资源'
     },
     r155: {
         api: 'https://155api.com',
-        name: '155资源',
-        adult: true
+        name: '155资源'
     },
     lsb: {
         api: 'https://apilsbzy1.com',
-        name: 'lsb资源',
-        adult: true
+        name: 'lsb资源'
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
         detail: 'https://hsckzy.vip'
     },
     yutu: {
